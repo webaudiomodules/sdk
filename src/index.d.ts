b0837251a508748f20156f0d09807b2dd5e499c7
Fr0stbyteR <renshihong@hotmail.com>
2021-10-26 01:53:30 +0800

    export types

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,3 +2,4 @@ export { default as WebAudioModule } from './WebAudioModule.js';
 export { default as WamParameterInfo } from './WamParameterInfo.js';
 export { default as WamNode } from './WamNode.js';
 // export { default as WamProcessor } from './WamProcessor.js';
+export * from "./types";

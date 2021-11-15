a66e56568677522d4119aa243e8263287ba426df
Fr0stbyteR <renshihong@hotmail.com>
2021-11-16 01:18:51 +0800

    npm updated

--- src/WamEnv.d.ts
+++ src/WamEnv.d.ts
@@ -1,3 +1,5 @@
-declare const initializeWamEnv: (apiVersion: string) => void;
+import { WamEnv } from "@webaudiomodules/api";
+
+declare const initializeWamEnv: (apiVersion: string) => typeof WamEnv;
 
 export default initializeWamEnv;

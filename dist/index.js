5dfd758ae00dd11b7d365e90f86b55ebe5f389b2
Fr0stbyteR <renshihong@hotmail.com>
2021-12-17 01:10:24 +0800

    add initializeWamGroup into index

--- dist/index.js
+++ dist/index.js
@@ -376,6 +376,96 @@ var initializeWamEnv = (apiVersion) => {
 };
 var WamEnv_default = initializeWamEnv;
 
+// src/WamGroup.js
+var initializeWamGroup = (groupId, groupKey) => {
+  const audioWorkletGlobalScope = globalThis;
+  class WamGroup {
+    constructor(groupId2, groupKey2) {
+      this._groupId = groupId2;
+      this._validate = (key) => {
+        return key == groupKey2;
+      };
+      this._eventGraph = /* @__PURE__ */ new Map();
+      this._processors = /* @__PURE__ */ new Map();
+    }
+    get groupId() {
+      return this._groupId;
+    }
+    validate(groupKey2) {
+      return this._validate(groupKey2);
+    }
+    addWam(wam) {
+      this._processors.set(wam.instanceId, wam);
+    }
+    removeWam(wam) {
+      if (this._eventGraph.has(wam))
+        this._eventGraph.delete(wam);
+      this._eventGraph.forEach((outputMap) => {
+        outputMap.forEach((set) => {
+          if (set && set.has(wam))
+            set.delete(wam);
+        });
+      });
+      this._processors.delete(wam.instanceId);
+    }
+    connectEvents(fromId, toId, output) {
+      const from = this._processors.get(fromId);
+      const to = this._processors.get(toId);
+      let outputMap;
+      if (this._eventGraph.has(from)) {
+        outputMap = this._eventGraph.get(from);
+      } else {
+        outputMap = [];
+        this._eventGraph.set(from, outputMap);
+      }
+      if (outputMap[output]) {
+        outputMap[output].add(to);
+      } else {
+        const set = /* @__PURE__ */ new Set();
+        set.add(to);
+        outputMap[output] = set;
+      }
+    }
+    disconnectEvents(fromId, toId, output) {
+      const from = this._processors.get(fromId);
+      if (!this._eventGraph.has(from))
+        return;
+      const outputMap = this._eventGraph.get(from);
+      if (typeof toId === "undefined") {
+        outputMap.forEach((set) => {
+          if (set)
+            set.clear();
+        });
+        return;
+      }
+      const to = this._processors.get(toId);
+      if (typeof output === "undefined") {
+        outputMap.forEach((set) => {
+          if (set)
+            set.delete(to);
+        });
+        return;
+      }
+      if (!outputMap[output])
+        return;
+      outputMap[output].delete(to);
+    }
+    emitEvents(from, ...events) {
+      if (!this._eventGraph.has(from))
+        return;
+      const downstream = this._eventGraph.get(from);
+      downstream.forEach((set) => {
+        if (set)
+          set.forEach((wam) => wam.scheduleEvents(...events));
+      });
+    }
+  }
+  if (audioWorkletGlobalScope.AudioWorkletProcessor) {
+    audioWorkletGlobalScope.webAudioModules.addGroup(new WamGroup(groupId, groupKey));
+  }
+};
+var WamGroup_default = initializeWamGroup;
+
 // src/WamEventRingBuffer.js
 var getWamEventRingBuffer = (moduleId) => {
   const audioWorkletGlobalScope = globalThis;
@@ -1655,6 +1745,7 @@ export {
   WamParameterInfo_default as getWamParameterInfo,
   WamParameterInterpolator_default as getWamParameterInterpolator,
   WamProcessor_default as getWamProcessor,
-  WamEnv_default as initializeWamEnv
+  WamEnv_default as initializeWamEnv,
+  WamGroup_default as initializeWamGroup
 };
 //# sourceMappingURL=index.js.map
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ export { default as WebAudioModule } from './WebAudioModule.js';
 export { default as getRingBuffer } from './RingBuffer.js';
 export { default as getWamArrayRingBuffer } from './WamArrayRingBuffer.js';
 export { default as initializeWamEnv } from './WamEnv.js';
+export { default as initializeWamGroup } from './WamGroup.js';
 export { default as getWamEventRingBuffer } from './WamEventRingBuffer.js';
 export { default as WamNode } from './WamNode.js';
 export { default as getWamParameter } from './WamParameter.js';
